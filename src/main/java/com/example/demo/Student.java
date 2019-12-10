0ecc496f1fde9a2646f6ea75a689e037790906e8
johnsuh01 <johnsuh01@hotmail.com>
2019-12-10 11:44:39 -0500

    SpringBoot_202 and Exercise_202

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+public class Student {
+
+
+    private String name;
+    private int age;
+    private String height;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    private String phone_number;
+
+}
